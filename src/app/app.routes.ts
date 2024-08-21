2e55cdf0d82b618b64da30e6d10c9a6636b4db6b
BrunoStepanescu <bruno.stepanescu@gmail.com>
2024-08-21 20:21:25 +0200

    Added all pages and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,26 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 
-export const routes: Routes = [{ path: '', component: HomeComponent }];
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  {
+    path: 'destination',
+    loadComponent: () =>
+      import('./pages/destination/destination.component').then(
+        (m) => m.DestinationComponent
+      ),
+  },
+  {
+    path: 'crew',
+    loadComponent: () =>
+      import('./pages/crew/crew.component').then((m) => m.CrewComponent),
+  },
+  {
+    path: 'technology',
+    loadComponent: () =>
+      import('./pages/technology/technology.component').then(
+        (m) => m.TechnologyComponent
+      ),
+  },
+];
